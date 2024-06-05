175d94b1621482f2e0faf6d4922beb6d149efff3
dmitriy.frostoff <dmitriy.frostoff@yandex.ru>
2024-06-05 13:39:41 +0300

    chore: update boilerplate's packages

--- configs/webpack/webpack.config.ts
+++ configs/webpack/webpack.config.ts
@@ -133,41 +133,46 @@ const config: Configuration = {
     minimizer: [
       "...",
       new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            // Lossless optimization with custom option
-            // Feel free to experiment with options for better result for you
-            plugins: [
-              ["gifsicle", { interlaced: true }],
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-              // Svgo configuration here https://github.com/svg/svgo#configuration
-              [
-                "svgo",
-                {
-                  plugins: [
-                    {
-                      name: "preset-default",
-                      params: {
-                        overrides: {
-                          removeViewBox: false,
-                          addAttributesToSVGElement: {
-                            params: {
-                              attributes: [
-                                { xmlns: "http://www.w3.org/2000/svg" },
-                              ],
-                            },
+        minimizer: [
+          {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              // Lossless optimization with custom option
+              // Feel free to experiment with options for better result for you
+              plugins: [
+                ["gifsicle", { interlaced: true }],
+                ["jpegtran", { progressive: true }],
+                ["optipng", { optimizationLevel: 5 }],
+              ],
+            },
+          },
+          {
+            implementation: ImageMinimizerPlugin.svgoMinify,
+            options: {
+              encodeOptions: {
+                // Pass over SVGs multiple times to ensure all optimizations are applied. False by default
+                multipass: true,
+                plugins: [
+                  {
+                    name: "preset-default",
+                    params: {
+                      overrides: {
+                        removeViewBox: false,
+                        addAttributesToSVGElement: {
+                          params: {
+                            attributes: [
+                              { xmlns: "http://www.w3.org/2000/svg" },
+                            ],
                           },
                         },
                       },
                     },
-                  ],
-                },
-              ],
-            ],
+                  },
+                ],
+              },
+            },
           },
-        },
+        ],
       }),
     ],
   },
